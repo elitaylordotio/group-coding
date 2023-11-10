39fdd22b4c80410460a06a153de58aaca4dec318
Eli Taylor <zach@elitaylor.io>
2023-11-09 16:09:22 -0800

    Add tests for min, and add function to test to ease popping

--- typescript/src/Cracking the Coding Interview/3.2/minStack.test.ts
+++ typescript/src/Cracking the Coding Interview/3.2/minStack.test.ts
@@ -1,5 +1,11 @@
 import { Stack } from "./minStack";
 
+function popMultiple(stack: Stack, popAmount: number) {
+    for (let i = 0; i < popAmount; i++) {
+        stack.pop();
+    }
+}
+
 describe("Min Stack", () => {
     test("can initialize a stack", () => {
         const array: number[] = [];
@@ -17,8 +23,8 @@ describe("Min Stack", () => {
 
             let lengthOfArray = Math.round(Math.random() * 20)
 
-            for(let i = 0; i < lengthOfArray; i++){
-                let j = Math.round(Math.random() * 100) 
+            for (let i = 0; i < lengthOfArray; i++) {
+                let j = Math.round(Math.random() * 100)
                 stack.push(j);
                 testArray.push(j);
             }
@@ -29,11 +35,11 @@ describe("Min Stack", () => {
     describe("can pop", () => {
         test("multiple values", () => {
             const array: number[] = [];
-            const testArray: number[] = [2,2,3,6,1,4,6,9,4,7,8];
+            const testArray: number[] = [2, 2, 3, 6, 1, 4, 6, 9, 4, 7, 8];
 
             const stack = new Stack(array);
 
-            for(let i = 0; i < testArray.length; i++){
+            for (let i = 0; i < testArray.length; i++) {
                 stack.push(testArray[i]);
             }
 
@@ -44,6 +50,54 @@ describe("Min Stack", () => {
             expect(array).toEqual(testArray.slice(0, -2))
             expect(stack.pop()).toEqual(4);
             expect(array).toEqual(testArray.slice(0, -3))
+        });
+    });
+    describe("can get min value", () => {
+        test("multiple values", () => {
+            const array: number[] = [];
+            const testArray: number[] = [2, 2, 3, 6, 1, 4, 6, 9, 1, 7, 8];
+
+            const stack = new Stack(array);
+
+            for (let i = 0; i < testArray.length; i++) {
+                stack.push(testArray[i]);
+            }
+
+            expect(array).toEqual(testArray);
+            expect(stack.min()).toEqual(1);
+
+            popMultiple(stack, 3);
+            expect(stack.min()).toEqual(1);
+            expect(array).toEqual(testArray.slice(0, -3));
+
+            popMultiple(stack, 3);
+            expect(stack.min()).toEqual(1);
+            expect(array).toEqual(testArray.slice(0, -6));
+
+            popMultiple(stack, 1);
+            expect(stack.min()).toEqual(2);
+            expect(array).toEqual(testArray.slice(0, -7));
+
+            popMultiple(stack, 3);
+            expect(stack.min()).toEqual(2);
+
+            popMultiple(stack, 1);
+            expect(array.length).toEqual(0);
+        });
+        test("after adding a value", () => {
+            const array: number[] = [];
+            const testArray: number[] = [5, 5, 3, 6, 11, 4, 6, 9, 15, 7, 8];
+
+            const stack = new Stack(array);
+
+            for (let i = 0; i < testArray.length; i++) {
+                stack.push(testArray[i]);
+            }
+
+            expect(stack.pop()).toEqual(8);
+            expect(stack.min()).toEqual(3);
+            stack.push(2);
+            expect(stack.min()).toEqual(2);
         });
     });
 });
