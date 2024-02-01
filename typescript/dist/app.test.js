f9bd417488fe0bf0888577fe074f0cc6ef329769
Eli Taylor <zach@elitaylor.io>
2024-01-31 18:12:37 -0800

    Remove dist and ignore it

--- typescript/dist/app.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/app.test.ts
-const app_1 = require("./app");
-test('adds 1 + 2 to equal 3', () => {
-    expect((0, app_1.add)(1, 2)).toBe(3);
-});
